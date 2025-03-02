64f9998c3ef9e467810cd66bf901ab83c8d0d674
MO Slah <88237350+m0slah@users.noreply.github.com>
2025-03-02 23:38:20 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Swiper from "./components/Swiper";
+
+function App() {
+  return (
+    <div>
+      <Swiper />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Swiper.tsx
@@ -0,0 +1,3 @@
+export default function Swiper() {
+  return <div>Swiper</div>;
+}
